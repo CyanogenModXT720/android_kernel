384ffbe6849b909b676d24c1f872af19fba9fae2
Thai Leav <tleav@sig-ubuntu-2.(none)>
2009-06-22 12:56:57 -0500

    LIBss60233 SSMgr / Modem PM Driver K29 Upmerge

---
+++ drivers/misc/modem_pm_driver/modem_pm_driver_main.c
@@ -0,0 +1,245 @@
+/*
+ * Copyright (C) 2009, Motorola, All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA
+ *
+ * Motorola 2009-Jan-28 - Initial Creation
+ */
+
+#include <linux/err.h>
+#include <linux/errno.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/poll.h>
+#include <linux/semaphore.h>
+#include <mach/resource.h>
+#include "modem_pm_driver.h"
+
+static char modem_pm_driver_opened;
+
+/* static struct constraint_handle *co_latency; */
+/* static struct constraint_handle *co_vdd2_opp; */
+
+static struct class *modem_pm_driver_class;
+static struct device *modem_pm_driver_dev;
+
+static int modem_pm_driver_major_num;
+
+static DECLARE_MUTEX(modem_pm_driver_lock);
+
+
+static int modem_pm_driver_init(void);
+
+static void modem_pm_driver_exit(void);
+
+static int modem_pm_driver_open(struct inode *inode, struct file *file);
+
+static int modem_pm_driver_free(struct inode *inode, struct file *file);
+
+static int modem_pm_driver_ioctl(struct inode *inode,
+				 struct file *file,
+				 unsigned int cmd,
+				 unsigned long arg);
+
+/* This structure defines the file operations for the Modem PM Driver */
+static struct file_operations modem_pm_driver_fops = {
+	.owner =    THIS_MODULE,
+	.ioctl =    modem_pm_driver_ioctl,
+	.open =     modem_pm_driver_open,
+	.release =  modem_pm_driver_free
+};
+
+static int modem_pm_driver_init(void)
+{
+	int retval = 0;
+
+	/* Register the character device. */
+	modem_pm_driver_major_num =
+		register_chrdev(0, MODEM_PM_DRIVER_DEV_NAME,
+				&modem_pm_driver_fops);
+
+	/* If the character device is registered, continue. */
+	if (modem_pm_driver_major_num >= 0) {
+		/* Make the character device. */
+		modem_pm_driver_class = class_create(THIS_MODULE,
+						     MODEM_PM_DRIVER_DEV_NAME);
+
+		if (IS_ERR(modem_pm_driver_class)) {
+			unregister_chrdev(modem_pm_driver_major_num,
+					  MODEM_PM_DRIVER_DEV_NAME);
+			retval = -EFAULT;
+		} else {
+			modem_pm_driver_dev =
+				device_create(modem_pm_driver_class,
+					      NULL,
+					      MKDEV(modem_pm_driver_major_num,
+						    0),
+					      NULL,
+					      MODEM_PM_DRIVER_DEV_NAME);
+
+			if (IS_ERR(modem_pm_driver_dev)) {
+				class_destroy(modem_pm_driver_class);
+				unregister_chrdev(modem_pm_driver_major_num,
+						  MODEM_PM_DRIVER_DEV_NAME);
+				retval = -EFAULT;
+			}
+		}
+	}
+	/* If we failed to get a character device, return the error. */
+	else
+		retval = modem_pm_driver_major_num;
+
+	return retval;
+}
+
+static void modem_pm_driver_exit(void)
+{
+	/* Unregister the character device */
+	device_destroy(modem_pm_driver_class,
+		       MKDEV(modem_pm_driver_major_num, 0));
+	class_destroy(modem_pm_driver_class);
+	unregister_chrdev(modem_pm_driver_major_num, MODEM_PM_DRIVER_DEV_NAME);
+}
+
+static int modem_pm_driver_open(struct inode *inode, struct file *file)
+{
+	int retval = -EINTR;
+
+	/* Acquire the mutex */
+	if (down_interruptible(&modem_pm_driver_lock) == 0) {
+		/* Check if it is not already open */
+		if (modem_pm_driver_opened == 0) {
+			/* Set the opened state */
+			modem_pm_driver_opened = 1;
+			retval = 0;
+
+			/* Get the VDD2 OPP constraint handle */
+			/* co_vdd2_opp = constraint_get(
+			   MODEM_PM_DRIVER_DEV_NAME,
+			   &cnstr_id_vdd2_opp); */
+
+			/* Get the latency constraint handle */
+			/* co_latency = constraint_get(MODEM_PM_DRIVER_DEV_NAME,
+			   &cnstr_id_latency); */
+		} else
+			retval = -EBUSY;
+
+		/* Release the mutex */
+		up(&modem_pm_driver_lock);
+	}
+
+	return retval;
+}
+
+static int modem_pm_driver_free(struct inode *inode, struct file *file)
+{
+	int retval = -EINTR;
+
+	/* Acquire the mutex */
+	if (down_interruptible(&modem_pm_driver_lock) == 0) {
+
+		/* Remove the setting of the VDD2 OPP constraint */
+		/* constraint_remove(co_vdd2_opp); */
+
+		/* Release the VDD2 OPP constraint handle */
+		/* constraint_put(co_vdd2_opp); */
+
+		/* Remove the setting of the VDD2 OPP constraint */
+		/* constraint_remove(co_latency); */
+
+		/* Release the latency constraint handle */
+		/* constraint_put(co_latency); */
+
+		modem_pm_driver_opened = 0;
+		retval = 0;
+
+		/* Release the mutex */
+		up(&modem_pm_driver_lock);
+	}
+
+	return retval;
+}
+
+static int modem_pm_driver_ioctl(struct inode *inode,
+				 struct file *file,
+				 unsigned int cmd,
+				 unsigned long arg)
+{
+	int status = -EFAULT;
+
+	switch (cmd) {
+	case MODEM_PM_DRIVER_IOCTL_HANDLE_FREQUENCY_OPP_CONSTRAINT: {
+		switch (arg) {
+		case MODEM_PM_SHARED_DDR_FREQUENCY_OPP_HIGH: {
+			/* Set the target value of the VDD2 OPP constraint */
+			/* constraint_set(co_vdd2_opp, CO_VDD2_OPP3); */
+			status = 0;
+			break;
+		}
+		case MODEM_PM_SHARED_DDR_FREQUENCY_OPP_NO_VOTE: {
+			/* Remove the setting of the VDD2 OPP constraint */
+			/* constraint_remove(co_vdd2_opp); */
+			status = 0;
+			break;
+		}
+		default:
+			break;
+		}
+		break;
+	}
+	case MODEM_PM_DRIVER_IOCTL_HANDLE_LOW_POWER_POLICY_CONSTRAINT: {
+		switch (arg) {
+		case MODEM_PM_SHARED_DDR_LOW_POWER_POLICY_ON_INACTIVE: {
+			/* Set the target value of the latency constraint */
+			/* constraint_set(co_latency,
+			   CO_LATENCY_MPUOFF_COREON); */
+			status = 0;
+			break;
+		}
+		case MODEM_PM_SHARED_DDR_LOW_POWER_POLICY_RET: {
+			/* Set the target value of the latency constraint */
+			/* constraint_set(co_latency,
+			   CO_LATENCY_MPUOFF_CORERET); */
+			status = 0;
+			break;
+		}
+		case MODEM_PM_SHARED_DDR_LOW_POWER_POLICY_NO_VOTE: {
+			/* Remove the setting of the latency constraint */
+			/* constraint_remove(co_latency); */
+			status = 0;
+			break;
+		}
+		default:
+			break;
+		}
+		break;
+	}
+	default:
+		break;
+	}
+
+	return status;
+}
+
+/* Module entry points */
+module_init(modem_pm_driver_init);
+module_exit(modem_pm_driver_exit);
+
+MODULE_DESCRIPTION("Modem PM Driver");
+MODULE_AUTHOR("Motorola");
+MODULE_LICENSE("GPL");
+
