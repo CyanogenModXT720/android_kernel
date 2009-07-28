b25c04693d5d082393aba1100a52f95e21d3dc1a
Supreet Bakshi <pcj784@il93lnxgit02.(none)>
2009-07-28 14:08:11 -0500

    k29_try

--- drivers/sim/sim_core.c
+++ drivers/sim/sim_core.c
@@ -17,6 +17,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  
  * 02111-1307, USA
  *
+ * Motorola 2009-Jul-28 - Porting suspend/resume functionality
  * Motorola 2009-Jun-17 - Adding clock framework changes
  * Motorola 2009-Jun-01 - Adding power regulator changes
  * Motorola 2009-Mar-30 - Workaround 1.8V issue
