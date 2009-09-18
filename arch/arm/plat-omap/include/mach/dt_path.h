07f805070c81095ea0753b32733db5b47bfe7863
janboe <yuan-bo.ye@motorola.com>
2009-09-18 17:07:59 +0800

    disable keypad which is covered by screen when slide is closed.
    Signed-off-by: janboe <yuan-bo.ye@motorola.com>

--- arch/arm/plat-omap/include/mach/dt_path.h
+++ arch/arm/plat-omap/include/mach/dt_path.h
@@ -42,6 +42,7 @@
 #define DT_PROP_KEYPAD_COLREG	"columnregister"
 #define DT_PROP_KEYPAD_MAPNUM	"mapnum"
 #define DT_PROP_KEYPAD_MAPS	"maps"
+#define DT_PROP_KEYPAD_CLOSED_MAPS "closed_maps"
 #define DT_PROP_KEYPAD_NAME	"name"
 
 /* GPIODev Node */
